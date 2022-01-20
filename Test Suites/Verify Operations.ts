872e4af2472ff89c8b9eae6ea569f0a7ae772144
Coty Rosenblath <coty@pobox.com>
2022-01-19 21:04:35 -1000

    Update Test Suites and Collections

--- Test Suites/Verify Operations.ts
+++ Test Suites/Verify Operations.ts
@@ -11,4 +11,28 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>437cc529-6e28-4c94-ae29-e1709755d800</testSuiteGuid>
+   <testCaseLink>
+      <guid>9953cf4c-8dea-4df4-9240-ccf83477b0e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/AD2-1 Add numbers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>71eae780-7bd7-4873-ab46-2b5ae42df3b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/AD2-2 Subtract numbers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a462bdcc-8fe8-4134-975c-629611c473f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/AD2-3 Multiply numbers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>37243630-08ad-4b4a-99bb-930ecc002587</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/operations/AD2-4 Divide numbers</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
